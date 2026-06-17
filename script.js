db6fff37a8a6684aeaa745388131b59fd7b062dd
acesavvy101 <gracielladeronne07@gmail.com>
2026-06-18 00:52:33 +0800

    feat: display book cards from library data

--- script.js
+++ script.js
@@ -16,5 +16,27 @@ function addBookToLibrary (BookInstance) {
 }
 
 const Book1 = new Book ("The Secret History", "Donna Tartt", 600 , true);
+const Book2 = new Book ("The Invention of Morel", "Adolfo Bioy Casares" , 120, true);
+const Book3 = newBook ("The Kites" , "Romain Gary", 320, false);
+
+const cardContainer = document.getElementById("container");
+function displayBook (array) {
+    for (const book of array) {
+        const card = document.createElement("div");
+        const cardText = document.createElement("p");
+
+        //u want to display the PROPERTIES of an obj 
+        cardText.textContent = //youre assining one string (with 4 lines) to textContent 
+            `Title: ${book.title}
+            Author: ${book.author}
+            Page Number: ${book.pageNo}
+            Read Status: ${book.isRead}`
+        //the text content should be each of these books and their attributes
+
+        card.append(cardText);
+        cardContainer.append(card);
+    }
+
+}
 
 
